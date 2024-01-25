bf332dea8862e51786029e40a7a354de9a869f32
Diabl-o <dangalsubinam@gmail.com>
2024-01-25 13:16:14 +0545

    first commit

---
+++ home.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+    
+    <header class="navbar">
+        <div class="nav_in">
+            <div class="nav_left">
+                <img src="images/icon.jpg">
+            </div>
+            <ul class="nav_middle">
+                <li class="pad">
+                <a href="home.php">Home</a>
+                </li>
+                <li class="pad">
+                <a href="blog.php">Blog</a>
+                </li>
+                <li class="pad">
+                <a href="5lovelanguage.php">5 Love Languages</a>
+                </li>
+                <li class="pad">
+                <a href="compatibilitytest.php">Compatibility Test</a>
+                </li>
+                <li class="pad">
+                <a href="about.php">About</a>
+                </li>
+            </ul>
+            <div class="nav_right">
+                <div class="menu">
+                 <button class="nav_button">   
+                <i class="fa-solid fa-bars icon"></i>
+                 </button>
+                </div>
+            </div>
+        </div>
+
+    </header>
+
+    <div class="main">
+        <div class="card">
+            <div class="card_main">
+                <div>
+                    <p class="a"><b>17,859</b> tests taken in the last 30 days</p>
+                    <p class="b">Ready to crack the code of love? Dive into our snappy 5-minute quiz! Inspired by love guru Dr. Gary Chapman, it's all about love languages - your secret love dialect. Discover how you and your partner really tick in the language of love. It’s a quick, enlightening ride to understanding your unique love style. Spark up better chats and deeper connections. Start now and give your love life a fun upgrade! 🚀❤️🔍</p>
+                </div>
+                <h2 class="b mb fs">Discover Your Love Language:</h2>
+                <div class="loaded mb">
+                    <div class="load">
+
+                    </div>
+                </div>
+                <div class="question_container">
+                    <div class="question mb">
+                        <div class="question_text b mbo">
+                        I feel connected to my partner through a hug or a kiss.
+                        </div>
+                        <div class="question_button">
+                        <button class="qb">1</button>
+                        <button class="qb">2</button>
+                        <button class="qb">3</button>
+                        <button class="qb">4</button>
+                        <button class="qb">5</button>
+                        </div>
+                        <div class="question_response a mbo fn">
+                            <div>STRONGLY DISAGREE</div>
+                            <div>STRONGLY AGREE</div>
+                        </div>
+                    </div>
+                    <div class="question mb">
+                        <div class="question_text b mbo">
+                        I feel connected to my partner through a hug or a kiss.
+                        </div>
+                        <div class="question_button">
+                        <button class="qb">1</button>
+                        <button class="qb">2</button>
+                        <button class="qb">3</button>
+                        <button class="qb">4</button>
+                        <button class="qb">5</button>
+                        </div>
+                        <div class="question_response a mbo fn">
+                            <div>STRONGLY DISAGREE</div>
+                            <div>STRONGLY AGREE</div>
+                        </div>
+                    </div>
+                    <div class="question mb">
+                        <div class="question_text b mbo">
+                        I feel connected to my partner through a hug or a kiss.
+                        </div>
+                        <div class="question_button">
+                        <button class="qb">1</button>
+                        <button class="qb">2</button>
+                        <button class="qb">3</button>
+                        <button class="qb">4</button>
+                        <button class="qb">5</button>
+                        </div>
+                        <div class="question_response a mbo fn">
+                            <div>STRONGLY DISAGREE</div>
+                            <div>STRONGLY AGREE</div>
+                        </div>
+                    </div>
+                    <div class="question mb">
+                        <div class="question_text b mbo">
+                        I feel connected to my partner through a hug or a kiss.
+                        </div>
+                        <div class="question_button">
+                        <button class="qb">1</button>
+                        <button class="qb">2</button>
+                        <button class="qb">3</button>
+                        <button class="qb">4</button>
+                        <button class="qb">5</button>
+                        </div>
+                        <div class="question_response a mbo fn">
+                            <div>STRONGLY DISAGREE</div>
+                            <div>STRONGLY AGREE</div>
+                        </div>
+                    </div>
+                    <div class="question mb">
+                        <div class="question_text b mbo">
+                        I feel connected to my partner through a hug or a kiss.
+                        </div>
+                        <div class="question_button">
+                        <button class="qb">1</button>
+                        <button class="qb">2</button>
+                        <button class="qb">3</button>
+                        <button class="qb">4</button>
+                        <button class="qb">5</button>
+                        </div>
+                        <div class="question_response a mbo fn">
+                            <div>STRONGLY DISAGREE</div>
+                            <div>STRONGLY AGREE</div>
+                        </div>
+                    </div>
+
+                    <div class="next_container">
+                        <div>
+                            <button class="next_button">Next</button>
+                        </div>
+                    </div>
+
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="about_container">
+            <div class="about_card">
+                <div class="about_head">
+                    <h2  class="ab_head b">Love Language Test</h2>
+                </div>
+                <div class="about_main">
+                    <div>
+                        <h2 class="b">About the Love Language Test</h2>
+                        <p class="a">The Love Language Test is an insightful and interactive tool that aims to reveal the unique ways you prefer to give and receive love. The concept of 'love languages' suggests that people have distinct preferences in how they express and experience affection. This test, through a series of carefully crafted questions, helps you uncover your primary and secondary love languages - the dominant ways you communicate and perceive love. Whether it's through words of affirmation, acts of service, receiving gifts, quality time, or physical touch, understanding your love languages can significantly enhance your interpersonal connections and enable you to convey your feelings more effectively and meaningfully to those you cherish.</p>
+                    </div>
+                    <div>
+                        <h2 class="b">How Is the Analysis Made?</h2>
+                        <p class="a">The Love Language Test analyzes your responses to the questions you answered during the test. Each question corresponds to one of the five love languages: Words of Affirmation, Acts of Service, Receiving Gifts, Quality Time, and Physical Touch. Based on your selected answers, the test calculates the frequency of each love language that resonates with you the most.<br>
+
+                            The love language with the highest frequency is considered your primary love language, which represents the way you most strongly feel loved and appreciated. The love language with the second-highest frequency is your secondary love language, providing additional insight into how you perceive and express love.<br>
+
+                            The analysis aims to offer you a deeper understanding of your emotional needs, preferences, and communication style when it comes to giving and receiving love. It serves as a tool to enhance self-awareness and improve your relationships by aligning your expressions of love with the love languages that resonate with you the most.</p>
+                    </div>
+                    <div>
+                        <h2 class="b">Is This a Compatibility Test?</h2>
+                        <p class="a">Contrary to a compatibility test that assesses how well two individuals might get along, the Love Language Test focuses solely on deciphering your personal love language preferences. It's not about matching you with someone else but rather about deepening your understanding of yourself. This understanding is crucial in all forms of relationships, be they romantic, familial, or friendships. By recognizing and appreciating your unique love language profile, you can foster stronger and more fulfilling relationships with those around you.</p>
+                    </div>
+                    <div>
+                        <h2 class="b">How Long Is This Test?</h2>
+                        <p class="a">The Love Language Test consists of a series of thought-provoking questions, each designed to tap into your preferences and experiences regarding love and affection. The duration of the test varies depending on how quickly you reflect on and respond to each question. Generally, it takes about 5-10 minutes to complete. This short investment of time can provide valuable insights into your emotional communication style.</p>
+                    </div>
+                    <div>
+                        <h2 class="b">Is This Test Really Free?</h2>
+                        <p class="a">Yes, the Love Language Test is offered completely free of charge. We believe in making self-awareness and personal growth accessible to everyone. Therefore, there are no fees, hidden or otherwise, associated with this test. Our primary goal is to provide you with valuable insights into your love language preferences without any financial barriers.</p>
+                    </div>
+                    <div>
+                        <h2 class="b">What Will My Results for the Love Styles Test Look Like?</h2>
+                        <p class="a">Upon completing the Love Language Test, you will receive a comprehensive report detailing your primary and secondary love languages. This report includes not only a description of each love language but also real-world examples and scenarios that illustrate how these preferences manifest in your daily interactions. These results aim to enhance your understanding of your emotional needs and how you connect with others, offering a foundation for stronger and more empathetic relationships.</p>
+                    </div>
+                    <div>
+                        <h2 class="b">Is This Personality Test Appropriate for Children?</h2>
+                        <p class="a">The Love Language Test is designed to be inclusive and accessible for individuals of various ages, including children. While children can certainly benefit from gaining insights into their emotional expression, it is advisable for younger children to have the guidance and support of parents or guardians. This assistance ensures that they can understand the questions and accurately express their feelings and preferences.</p>
+                    </div>
+                    <div>
+                        <h2 class="b">Are You Going to Sell My Data?</h2>
+                        <p class="a">We are committed to upholding the highest standards of privacy and confidentiality. The information you provide during the Love Language Test is used exclusively for the purpose of generating your personalized results. We firmly stand against selling or sharing your personal data with third parties. Your trust is important to us, and we are dedicated to protecting your personal information and ensuring your privacy throughout the testing process.</p>
+                    </div>
+                    <hr class="foot">
+
+                    <div>
+                    <h2 class="b">Notes</h2>
+                    <ul class="a notes fn1">
+                        <li>
+                        The Love Language Test is an informative tool designed to enhance your understanding of love languages. It does not replace professional advice or counseling.
+                        </li>
+                        <li>
+                        The results of the Love Language Test are meant to provide insights into your personal preferences and should be interpreted in the context of your unique experiences and relationships.
+                        </li>
+                    </ul>
+                    </div>
+
+                </div>
+                
+
+               
+            </div>
+
+        </div>
+
+    </div>
+
+    <footer class="main_foot">
+        <div class="col1">
+        © 2024 Love Language Test v1.4.2
+        </div>
+        <div class="col2">
+        Made by <span class="foot_s">  ♥</span> & Gopal Chitaure
+        </div>
+        <div class="col3">
+        <a href="home.php" class="foot_a">Home</a>
+        <span class="foot_s">|</span>
+        <a href="home.php" class="foot_a">Contact</a>
+        <span class="foot_s">|</span>
+        <a href="privacy.php" class="foot_a">Privacy Policy</a>
+        </div>
+
+    </footer>
+
+</body>
+</html>
